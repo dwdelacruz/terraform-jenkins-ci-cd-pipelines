dac8fce642ee9e1da464618c4da14dd6b5ffa5ae
dwdelacruz <82911283+dwdelacruz@users.noreply.github.com>
2022-01-11 10:05:04 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   profile    = "default"
   region     = "us-east-1"
-  access_key = "AKIA4OQCL5IGCNNXDNDF"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 
