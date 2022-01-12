8da522a52bded6dc6608f1798e990bf7b0cb38d7
dwdelacruz <82911283+dwdelacruz@users.noreply.github.com>
2022-01-12 17:39:51 -0500

    Adding VPC and Subnet
    Adding VPC and Subnet

--- main.tf
+++ main.tf
@@ -1,6 +1,22 @@
+resource "aws_vpc" "vpc1"{ 
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = { 
+    Name = "SNETTA-ec2-terraform-poc-vpc"
+  }
+}
+
+resource "aws_subnet" "sub1"{ 
+  vpc_id = aws_vpc.vpc1
+  cidr_block = "10.0.1.0/24"
+  tags = { 
+    Name = "SNETTA-ec2-terraform-poc-sub"
+  }
+}
+
 resource "aws_instance" "simple-servers" {
   ami                         = var.ami
-  subnet_id                   = var.SNETTA-PUBLIC-1.id
+  subnet_id                   = aws_subnet.sub1.id
   instance_type               = "t2.medium"
   associate_public_ip_address = true
   security_groups             = var.main_security_group
