b6bdc4bcac53fef3d935d23cce28b8fb9608055e
dwdelacruz <82911283+dwdelacruz@users.noreply.github.com>
2022-01-12 17:42:11 -0500

    VPC ID
    VPC ID

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "vpc1"{
 }
 
 resource "aws_subnet" "sub1"{ 
-  vpc_id = aws_vpc.vpc1
+  vpc_id = aws_vpc.vpc1.id
   cidr_block = "10.0.1.0/24"
   tags = { 
     Name = "SNETTA-ec2-terraform-poc-sub"
