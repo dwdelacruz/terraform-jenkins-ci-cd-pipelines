f429aecdfce3780395cdc9614f5a78dc75b885f0
dwdelacruz <82911283+dwdelacruz@users.noreply.github.com>
2022-01-13 14:19:42 -0500

    Commenting Script
    Commenting Script

--- main.tf
+++ main.tf
@@ -6,22 +6,22 @@ resource "aws_vpc" "vpc1"{
   }
 }
 
-resource "aws_subnet" "sub1"{
-  vpc_id = aws_vpc.vpc1.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "SNETTA-ec2-terraform-poc-sub"
-  }
-}
+#resource "aws_subnet" "sub1"{
+#  vpc_id = aws_vpc.vpc1.id
+#  cidr_block = "10.0.1.0/24"
+#  tags = {
+#    Name = "SNETTA-ec2-terraform-poc-sub"
+#  }
+#}
 
-resource "aws_instance" "simple-servers" {
-  ami                         = var.ami
-  subnet_id                   = aws_subnet.sub1.id
-  instance_type               = "t2.medium"
-  associate_public_ip_address = true
+#resource "aws_instance" "simple-servers" {
+#  ami                         = var.ami
+#  subnet_id                   = aws_subnet.sub1.id
+#  instance_type               = "t2.medium"
+#  associate_public_ip_address = true
   #security_groups             = var.sec_groups
-  key_name                    = "Terra"
-  tags = {
-    Name = "SNETTA-ec2-terraform-poc-kubectl-master-controller"
+#  key_name                    = "Terra"
+#  tags = {
+#    Name = "SNETTA-ec2-terraform-poc-kubectl-master-controller"
     #
-  }
+#  }
