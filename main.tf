c4c74b32f4ef6e6eac8b8d41ce655599e12721d2
dwdelacruz <82911283+dwdelacruz@users.noreply.github.com>
2022-01-13 14:26:59 -0500

    test
    test

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ resource "aws_vpc" "vpc1"{
   }
 }
 
-#resource "aws_subnet" "sub1"{
-#  vpc_id = aws_vpc.vpc1.id
-#  cidr_block = "10.0.1.0/24"
-#  tags = {
-#    Name = "SNETTA-ec2-terraform-poc-sub"
-#  }
-#}
+resource "aws_subnet" "sub1"{
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "SNETTA-ec2-terraform-poc-sub"
+  }
+}
 
 #resource "aws_instance" "simple-servers" {
 #  ami                         = var.ami
